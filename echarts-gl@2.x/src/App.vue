f9f6dcc0a076cc52ccb15233c47607950d726f94
anshengsunshine <646143658@qq.com>
2021-10-17 23:04:11 +0800

    新增地球飞线3D可视化

--- echarts-gl@2.x/src/App.vue
+++ echarts-gl@2.x/src/App.vue
@@ -1,16 +1,24 @@
 <template>
+  <!-- 3d 地球飞线图 -->
+  <fight-earth />
+  <!-- 3d 散点图 -->
+  <!-- <gL-bar-3D /> -->
   <!-- 3D 地图 -->
-  <gL-earth-3D />
+  <!-- <gL-earth-3D /> -->
 </template>
 
 <script>
-import GLEarth3D from "./echartsGLEarth3D";
+// import GLEarth3D from "./echartsGLEarth3D";
+// import GLBar3D from "./echartsGLBar3D";
+import FightEarth from "./FightEarth.vue"
 import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "HelloWorld",
   components: {
-    GLEarth3D,
+    // GLEarth3D,
+    // GLBar3D,
+    FightEarth,
   },
 });
 </script>
---
+++ echarts-gl@2.x/src/FightEarth.vue
@@ -0,0 +1,162 @@
+<template>
+  <v-chart class="chart" :option="options" />
+</template>
+
+<script>
+import { ref, onMounted } from "vue-demi";
+import { use } from "echarts";
+import { GlobeComponent } from "echarts-gl/components";
+import { Lines3DChart, Scatter3DChart } from "echarts-gl/charts";
+use([GlobeComponent, Lines3DChart, Scatter3DChart]);
+import VChart from "vue-echarts";
+
+export default {
+  name: "echartsGLEarth3D",
+  components: {
+    VChart,
+  },
+  setup() {
+    const options = ref({});
+    const update = () => {
+      fetch("http://www.youbaobao.xyz/datav-res/datav/flights.json")
+        .then((response) => response.json())
+        .then((data) => {
+          const airports = data.airports.map(function (item) {
+            return {
+              coord: [item[3], item[4]],
+            };
+          });
+          function getAirportCoord(idx) {
+            return [data.airports[idx][3], data.airports[idx][4]];
+          }
+          console.log(airports, data);
+          const routesGroupByAirline = {};
+          data.routes.forEach(function (route) {
+            const airline = data.airlines[route[0]];
+            const airlineName = airline[0];
+            if (!routesGroupByAirline[airlineName]) {
+              routesGroupByAirline[airlineName] = [];
+            }
+            routesGroupByAirline[airlineName].push(route);
+          });
+          const pointsData = [];
+          data.routes.forEach(function (airline) {
+            pointsData.push(getAirportCoord(airline[1]));
+            pointsData.push(getAirportCoord(airline[2]));
+          });
+          const series = data.airlines
+            .map(function (airline) {
+              const airlineName = airline[0];
+              const routes = routesGroupByAirline[airlineName];
+              if (!routes) {
+                return null;
+              }
+              return {
+                type: "lines3D",
+                name: airlineName,
+                effect: {
+                  show: true,
+                  trailWidth: 2,
+                  trailLength: 0.15,
+                  trailOpacity: 1,
+                  trailColor: "rgb(30, 30, 60)",
+                },
+                lineStyle: {
+                  width: 1,
+                  color: "rgb(50, 50, 150)",
+                  // color: 'rgb(118, 233, 241)',
+                  opacity: 0.1,
+                },
+                blendMode: "lighter",
+                data: routes.map(function (item) {
+                  return [airports[item[1]].coord, airports[item[2]].coord];
+                }),
+              };
+            })
+            .filter(function (series) {
+              return !!series;
+            });
+          series.push({
+            type: "scatter3D",
+            coordinateSystem: "globe",
+            blendMode: "lighter",
+            symbolSize: 2,
+            itemStyle: {
+              color: "rgb(50, 50, 150)",
+              opacity: 0.2,
+            },
+            data: pointsData,
+          });
+          options.value = {
+            legend: {
+              selectedMode: "single",
+              left: "left",
+              data: Object.keys(routesGroupByAirline),
+              orient: "vertical",
+              textStyle: {
+                color: "#fff",
+              },
+            },
+            globe: {
+              environment: require("@/assets/star-bg.jpg"),
+              baseTexture: require("@/assets/datav-gl-texture.jpg"),
+              heightTexture: require("@/assets/datav-gl-texture.jpg"),
+              displacementScale: 0.1,
+              displacementQuality: "high",
+              //   baseColor: "#000",
+              shading: "realistic",
+              realisticMaterial: {
+                roughness: 0.2,
+                metalness: 0,
+              },
+              postEffect: {
+                enable: true,
+                depthOfField: {
+                  enable: false,
+                  focalDistance: 150,
+                },
+              },
+              temporalSuperSampling: {
+                enable: true,
+              },
+              light: {
+                ambient: {
+                  intensity: 0,
+                },
+                main: {
+                  intensity: 5,
+                  shadow: false,
+                },
+                ambientCubemap: {
+                  texture: "@/asset/lake.hdr",
+                  exposure: 1,
+                  diffuseIntensity: 0.5,
+                  specularIntensity: 2,
+                },
+              },
+              viewControl: {
+                autoRotate: false,
+              },
+              silent: true,
+            },
+            series: series,
+          };
+        });
+    };
+    onMounted(() => {
+      update();
+    });
+    return {
+      options,
+      update,
+    };
+  },
+};
+</script>
+
+<style>
+.chart {
+  width: 100%;
+  height: 1200px;
+}
+</style>
